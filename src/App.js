9c098605bcbfb2ea57b98f9f8a65763ebf139cc5
btaras1 <btaras@foi.hr>
2021-05-31 19:03:35 +0200

    Hands-on

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react';
+import { AuthContext } from './context/AuthContext'
+import Articles from './components/Articles';
 
-function App() {
+const App = () => {
+  const { isLoggedIn, setIsLoggedIn} = useContext(AuthContext);
+
+  const handleAuth = () => {
+    setIsLoggedIn(!isLoggedIn);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handleAuth}>{isLoggedIn ? 'Logout' : 'Login'}</button>
+      <Articles />
+    </>
   );
 }
 
---
+++ src/components/Article.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+
+const Article = (props) => {
+    const { isLoggedIn } = useContext(AuthContext);
+    return(
+        <>
+            <h2>Some article title</h2>
+            <p>Description content...</p>
+            {isLoggedIn ? <button>Edit</button> : null}
+        </>
+    );
+}
+
+export default Article;
